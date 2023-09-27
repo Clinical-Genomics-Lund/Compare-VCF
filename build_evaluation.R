fe09105c2f09a258799c0cd6ad75331539806287
Jakob Willforss <jakob.willforss@hotmail.com>
2023-09-27 10:15:50 +0200

    Working on evaluation script

--- build_evaluation.R
+++ build_evaluation.R
@@ -1,19 +1,56 @@
 library(tidyverse)
 
-vcf1 <- read_tsv("data/23MD08256-jakob_test.scored.vcf.gz", comment="##", n_max=1000) %>%
+# Set the path
+setwd("\\\\wsl.localhost\\Ubuntu-22.04\\home\\jakob\\proj\\230920_rd_evaluate\\data")
+
+# vcf1_path <- "\\\\wsl.localhost\\Ubuntu-22.04\\home\\jakob\\proj\\230920_rd_evaluate\\data\\23MD08776-testrun.scored.vcf.gz"
+# vcf2_path <- "\\\\wsl.localhost\\Ubuntu-22.04\\home\\jakob\\proj\\230920_rd_evaluate\\data\\23MD08256-jakob_test.scored.vcf.gz"
+
+vcf1_path <- "23MD08776-testrun.scored.vcf.gz"
+vcf2_path <- "23MD08256-jakob_test.scored.vcf.gz"
+
+number_rows <- Inf
+
+vcf1 <- read_tsv(vcf1_path, comment="##", n_max=number_rows) %>%
 	rename(CHROM=`#CHROM`) %>%
 	mutate(pos=paste(CHROM, POS, sep=":")) %>%
 	mutate(mut=paste(REF, ALT, sep=",")) %>% 
-	mutate(rank=INFO %>% gsub(".*RankScore", "", .) %>% gsub(".*:", "", .) %>% gsub(";.*", "", .)) %>%
+	mutate(rank=INFO %>% gsub(".*RankScore", "", .) %>% gsub(".*:", "", .) %>% gsub(";.*", "", .) %>% as.numeric()) %>%
 	select(pos, mut, rank)
-vcf2 <- read_tsv("data/23MD08776-testrun.scored.vcf.gz", comment="##", n_max=1000) %>%
+vcf2 <- read_tsv(vcf2_path, comment="##", n_max=number_rows) %>%
 	rename(CHROM=`#CHROM`) %>%
 	mutate(pos=paste(CHROM, POS, sep=":")) %>%
 	mutate(mut=paste(REF, ALT, sep=",")) %>% 
-	mutate(rank=INFO %>% gsub(".*RankScore", "", .) %>% gsub(".*:", "", .) %>% gsub(";.*", "", .)) %>%
+	mutate(rank=INFO %>% gsub(".*RankScore", "", .) %>% gsub(".*:", "", .) %>% gsub(";.*", "", .) %>% as.numeric()) %>%
 	select(pos, mut, rank)
 
-# OK I have some first data!
-# Now join
-# And make a scatter chart
+# FIXME: Position might not be enough, the alternative allele?
+# FIXME: Many to many matches?
+joined_vcf <- vcf1 %>% full_join(vcf2, by="pos")
 
+joined_vcf_df <- as.data.frame(joined_vcf)
+
+# Scatter chart
+# FIXME: Investigate the outliers?
+# FIXME: Density?
+plt <- ggplot(joined_vcf_df %>% filter(!is.na(rank.x) & !is.na(rank.y)), aes(x=rank.x, y=rank.y)) + 
+	geom_point(na.rm=TRUE) + 
+	ggtitle("Compared rank scores")
+ggsave(filename="test.png", plt)
+
+# Histograms of rank distributions
+plt <- ggplot(joined_vcf_df %>% filter(!is.na(rank.x)), aes(x=rank.x)) + 
+	geom_histogram(na.rm=TRUE, bins=30) + 
+	ggtitle("Comparison 1 rank scores")
+ggsave(filename="hist1.png", plt)
+
+plt <- ggplot(joined_vcf_df %>% filter(!is.na(rank.y)), aes(x=rank.y)) + 
+	geom_histogram(na.rm=TRUE, bins=30) + 
+	ggtitle("Comparison 2 rank scores")
+ggsave(filename="hist2.png", plt)
+
+# Table
+rank_table <- joined_vcf %>% filter(rank.x >= 17 | rank.y >= 17) %>% arrange(desc(rank.x), desc(rank.y))
+rank_table
+
+write_tsv(rank_table, path="rank_table.tsv")

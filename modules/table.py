5143db31419480f6bb26d02840e66e87b69745f1
Jakob Willforss <jakob.willforss@hotmail.com>
2023-10-10 13:09:26 +0200

    First table version running

--- modules/table.py
+++ modules/table.py
@@ -9,7 +9,9 @@ def build_table(datasets: list[Dataset], top_n: int, outpath: str):
         else:
             return -1
 
-    var_key_tops = set()
+    all_top_keys = set()
+    top_keys_per_ds: dict[str, set[str]] = dict()
+
     datasets_w_scores = [ds for ds in datasets if ds.hasRankScores()]
     for ds in datasets_w_scores:
         # variant_keys = ds.getVariantKeys()
@@ -18,26 +20,32 @@ def build_table(datasets: list[Dataset], top_n: int, outpath: str):
         sorted_variants = sorted(variants, key=sort_fn, reverse=True)
         top_variants = sorted_variants[0:top_n]
         var_keys = [var.getKey() for var in top_variants]
-        var_key_tops.update(var_keys)
+        all_top_keys.update(var_keys)
+        top_keys_per_ds[ds.label] = set(var_keys)
 
     table_dict = dict()
-    table_dict["key"] = list(var_key_tops)
+    table_dict["key"] = list(all_top_keys)
+    score_labels = list()
     # var_is_present: list[list[bool]] = []
     # dataset_scores: list[list[int | None]] = []
     for ds in datasets_w_scores:
         ds_is_present = []
         ds_scores = []
-        for key in var_key_tops:
+        ds_among_top = []
+        for key in all_top_keys:
             var = ds.getVariantByKey(key)
             is_present = var is not None
             score = var.rankScore if is_present else None
+            in_top = key in top_keys_per_ds[ds.label]
 
             ds_is_present.append(is_present)
             ds_scores.append(score)
+            ds_among_top.append(in_top)
         table_dict[f"{ds.label}_present"] = ds_is_present
-        table_dict[f"{ds.label}_score"] = ds_scores
-        # var_is_present.append(ds_is_present)
-        # dataset_scores.append(ds_scores)
+        table_dict[f"{ds.label}_top{top_n}"] = ds_among_top
+        score_label = f"{ds.label}_score"
+        table_dict[score_label] = ds_scores
+        score_labels.append(score_label)
 
     # ordered_variants = list(all_variants)
 
@@ -52,8 +60,9 @@ def build_table(datasets: list[Dataset], top_n: int, outpath: str):
 
     # top_df = pd.DataFrame({"pos": positions, "mut": variant_strs, "score": scores})
     top_df = pd.DataFrame(table_dict)
-    top_df.sort_values(by=[f"{datasets_w_scores[0].label}_score"])
+    print(top_df)
+    print(score_labels)
+    top_df.sort_values(by=score_labels, inplace=True, ascending=False)
+    print(top_df)
 
     top_df.to_csv(outpath, sep="\t", index=False)
-
-    print(table_dict)

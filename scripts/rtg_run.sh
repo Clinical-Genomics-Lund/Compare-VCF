fc3f1f3d6a6b5699c0dfaa2ba879d19d9509ba07
Jakob Willforss <jakob.willforss@hotmail.com>
2023-10-10 15:55:33 +0200

    Omit scripts from repo

--- scripts/rtg_run.sh
+++
@@ -1,1 +0,0 @@
-~/src/bnf/^Cg-tools-3.12.1/rtg vcfeval --baseline 231004_giab_nist/HG002_GRCh38_1_22_v4.2.1_benchmark.vcf.gz --calls 231004_first_rd/nfcore_rd/giab_full_split_rmdup.chr.vcf.gz --output tenfcore_rd_results -t ../ref/GRCh38.sdf
